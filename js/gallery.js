6eda335874f644fb4e3423d9d5bb3ff2a38a1121
izattja <37969242+izattja@users.noreply.github.com>
2026-07-15 21:55:26 -0600

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -2,44 +2,55 @@
 var divColor;
 var divText;
 var divURL;
+const origText = "Hover over an image below to display here.";
+
+
+function setTabIndexes() {
+    console.log("Setting tab indexes for all preview images");
+    var currentImages = document.querySelectorAll(".preview");
+    var divImage = document.getElementById("image");
+    divImage.tabindex = 0;
+    var j = 1;
+    for (var i = 0; i < currentImages.length; i++) {
+        console.log("currentImages[" + i + "].tabIndex=" + j);
+        currentImages[i].tabIndex = j++;
+        currentImages[i].style.filter = 'blur(2px)';
+    }
+}
 
 function upDate(previewPic){
-    /*In this function you should 
+    /*
     1) change the url for the background image of the div with the id = "image" 
-    to the source file of the preview image*/
+    to the source file of the preview image
+    2) Change the text  of the div with the id = "image" to the alt text of the preview image
+    3) unblur the preview image by setting its css filter property to none
+    */ 
 
-      var divImage = document.getElementById("image");
-     
-      console.log("function upDate called **********")
-      console.log ("save original properties");
-      divText = divImage.innerHTML;
-      divColor = divImage.style.backgroundColor;
-      divURL = divImage.style.backgroundImage;
-      
-      console.log("change URL to:", previewPic.src);
-      divImage.style.backgroundImage = "url('" + previewPic.src + "')";
+    console.log("function upDate() called **********", previewPic.src);
+    var divImage = document.getElementById("image"); 
+    divColor = divImage.style.backgroundColor;
+    divText = divImage.innerHTML;
+    divURL = divImage.style.backgroundImage;
 
-    /*2) Change the text  of the div with the id = "image" 
-    to the alt text of the preview image*/ 
-      console.log("change text for div to:", previewPic.alt);
-      divImage.innerHTML = previewPic.alt;
+    divImage.style.backgroundImage = "url('" + previewPic.src + "')";
+    divImage.innerHTML = previewPic.alt;
+    previewPic.style.filter = 'none'
 }
 
-function unDo(){
-    /* In this function you should 
-    1) Update the url for the background image of the div with the id = "image" 
-    back to the orginal-image.  You can use the css code to see what that original URL was
-    */
-        var divImage = document.getElementById("image");
-        console.log("function unDo called **********");
-        console.log(divImage);
-
-        console.log("Restore original properties");
-        divImage.style.backgroundImage = divURL;
-        divImage.style.backgroundColor = divColor;
-
-    /*2) Change the text  of the div with the id = "image" 
+function unDo(previewPic){
+    /*
+    1) Update the url and background color for the background image of the div with the id = "image" 
+    back to the orginal-image.
+    2) Change the text  of the div with the id = "image" 
     back to the original text.  You can use the html code to see what that original text was
-    */
+    3) Blur the preview image by setting its css filter property to blur(2px)*/
+        console.log("function unDo() called **********");
+
+        var divImage = document.getElementById("image");
+        divImage.style.backgroundColor = divColor;
         divImage.innerHTML = divText;
+        divImage.style.backgroundImage = divURL;       
+        previewPic.style.filter = 'blur(2px)';
 }
+
+
